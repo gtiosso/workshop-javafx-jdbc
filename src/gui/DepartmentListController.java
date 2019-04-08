8b95e962ed6b4624e93e1a1d71f9b242e6dbcb21
Guilherme Tiosso <guilherme.tiosso@gmail.com>
2019-04-08 18:46:58 -0300

    DepartmentList Controller

---
+++ src/gui/DepartmentListController.java
@@ -0,0 +1,57 @@
+package gui;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import application.Main;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.stage.Stage;
+import model.entities.Department;
+
+public class DepartmentListController implements Initializable {
+
+	@FXML
+	private TableView<Department> tableViewDepartment;
+	
+	@FXML
+	private TableColumn<Department, Integer> tableColumnId;
+	
+	@FXML
+	private TableColumn<Department, String> tableColumnName;
+	
+	@FXML
+	private Button btNew;
+	
+	@FXML
+	public void onBtNewAction() {
+		System.out.println("onBtNewAction");
+	}
+	
+	@Override
+	public void initialize(URL uri, ResourceBundle rb) {
+		initializeNodes();
+		
+	}
+
+	// Metodo para iniciar o comportamento das colunas da TableView
+	private void initializeNodes() {
+		tableColumnId.setCellValueFactory(new PropertyValueFactory<>("id"));
+		tableColumnName.setCellValueFactory(new PropertyValueFactory<>("name"));
+		
+		// Pegando a referencia do Stage Principal, 
+		// atraves de um Downcasting da SuperClasse Window para a Classe Stage
+		Stage stage = (Stage) Main.getMainScene().getWindow();
+		
+		// Redimensionando tamanho da TableView para acompnhar a tela principal
+		tableViewDepartment.prefHeightProperty().bind(stage.heightProperty());
+		
+	}
+	
+	
+
+}
