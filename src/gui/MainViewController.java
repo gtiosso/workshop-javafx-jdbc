e514c29d39d8716179dcb900686384f0723d5178
Guilherme Tiosso <guilherme.tiosso@gmail.com>
2019-04-08 19:22:51 -0300

    Initializing action as parameter

--- src/gui/MainViewController.java
+++ src/gui/MainViewController.java
@@ -3,6 +3,7 @@ package gui;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.function.Consumer;
 
 import application.Main;
 import gui.util.Alerts;
@@ -35,54 +36,28 @@ public class MainViewController implements Initializable {
 	
 	@FXML
 	public void onMenuItemDepartmentAction() {
-		loadView2("/gui/DepartmentList.fxml");
+		loadView("/gui/DepartmentList.fxml", (DepartmentListController controller) -> {
+			controller.setDepartmentService(new DepartmentService());
+			controller.updateTableView();
+		});
 	}
 	
 	@FXML
 	public void onMenuItemAboutAction() {
-		loadView("/gui/About.fxml");
+		loadView("/gui/About.fxml", x -> {});
 	}
 	
 	
 	@Override
 	public void initialize(URL uri, ResourceBundle rb) {
-		
 
 	}
-	
 	
 	// Metodo para adicionar novas Views dentro da Cena Principal
 	// Synchronized = Garante que todo o processamento abaixo
 	// não será interrompido durante o multithread
 	@FXML
-	private synchronized void loadView(String absoluteName) {
-		try {
-			// FXMLLoader = carrega o arquivo da View.fxml
-			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
-			// Carrego a nova view em um atributo
-			VBox newVBox = loader.load();
-			
-			// Resgato a Cena Principal da View Principal do Programa
-			Scene mainScene = Main.getMainScene();
-			VBox mainVBox = (VBox) ((ScrollPane) mainScene.getRoot()).getContent();
-			
-			// Resgato os itens necessários para a apresentação da nova tela dentro da Cena Principal
-			// Resgato a Barra de Menu (Principal)
-			Node mainMenu = mainVBox.getChildren().get(0);
-			// Removo todos os itens da Cena Principal
-			mainVBox.getChildren().clear();
-			// Adiciono a Barra de Menu resgatada para a nova Tela
-			mainVBox.getChildren().add(mainMenu);
-			// Adiciono todos os novos itens a nova tela
-			mainVBox.getChildren().addAll(newVBox.getChildren());
-		}
-		catch (IOException e) {
-			Alerts.showAlert("IOException", "Error Load View", e.getMessage(), AlertType.ERROR);
-		}
-		
-	}
-	
-	private synchronized void loadView2(String absoluteName) {
+	private synchronized <T> void loadView(String absoluteName, Consumer<T> initializingAction) {
 		try {
 			// FXMLLoader = carrega o arquivo da View.fxml
 			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
@@ -103,16 +78,11 @@ public class MainViewController implements Initializable {
 			// Adiciono todos os novos itens a nova tela
 			mainVBox.getChildren().addAll(newVBox.getChildren());
 			
-			// Pegando a referencia para o Controller da View
-			// Processo manual para injeção da dependencia
-			DepartmentListController controller = loader.getController();
-			controller.setDepartmentService(new DepartmentService());
-			controller.updateTableView();
+			T controller = loader.getController();
+			initializingAction.accept(controller);
 		}
 		catch (IOException e) {
 			Alerts.showAlert("IOException", "Error Load View", e.getMessage(), AlertType.ERROR);
-		}
-		
+		}	
 	}
-
 }
