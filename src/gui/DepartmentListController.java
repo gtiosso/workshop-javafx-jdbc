2c63af22f7f57c4e56c99fbcb74fb37e5350d021
Guilherme Tiosso <guilherme.tiosso@gmail.com>
2019-04-08 20:30:30 -0300

    DepartmentForm (dialog) Design

--- src/gui/DepartmentListController.java
+++ src/gui/DepartmentListController.java
@@ -1,18 +1,28 @@
 package gui;
 
+
+import java.io.IOException;
 import java.net.URL;
 import java.util.List;
 import java.util.ResourceBundle;
 
 import application.Main;
+import gui.util.Alerts;
+import gui.util.Utils;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.Pane;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 import model.entities.Department;
 import model.services.DepartmentService;
@@ -36,9 +46,12 @@ public class DepartmentListController implements Initializable {
 	
 	private ObservableList<Department> obsList;
 	
+	
+	// ActionEvent = criando uma referencia para o controle que recebeu o evento 
 	@FXML
-	public void onBtNewAction() {
-		System.out.println("onBtNewAction");
+	public void onBtNewAction(ActionEvent event) {
+		Stage parentStage = Utils.currentStage(event);
+		createDialogForm("/gui/DepartmentForm.fxml", parentStage);
 	}
 	
 	// Criando acoplamento fraco, com injeção de dependencia e inversão de controle
@@ -76,6 +89,34 @@ public class DepartmentListController implements Initializable {
 		List<Department> list = service.findAll();
 		obsList = FXCollections.observableArrayList(list);
 		tableViewDepartment.setItems(obsList);
+	}
+	
+	private void createDialogForm(String absoluteName, Stage parentStage) {
+		// Instanciando a janela de dialogo
+		try {
+			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
+			Pane pane = loader.load();
+			
+			// Instanciando o novo Palco para a nova Janela
+			Stage dialogStage = new Stage();
+			// Declarando o Titulo da nova Janela
+			dialogStage.setTitle("Enter Department Data");
+			// Instanciando a nova Cena
+			dialogStage.setScene(new Scene(pane));
+			// Declarando que a nova janela não poderá ser redimensionada
+			dialogStage.setResizable(false);
+			// Declarando quem é o Palco "Pai" da nova janela
+			dialogStage.initOwner(parentStage);
+			// Declarando que a janela é do tipo Modal, ou seja
+			// enquanto esta não for fechada, não poderá interagir
+			// nas demais janelas
+			dialogStage.initModality(Modality.WINDOW_MODAL);
+			dialogStage.showAndWait();
+			
+		}
+		catch (IOException e) {
+			Alerts.showAlert("Exception", "Error Load View", e.getMessage(), AlertType.ERROR);
+		}
 	}
 
 }
