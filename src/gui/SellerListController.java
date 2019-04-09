3fe63e2a6d784a99d247a63805d111ac47651e83
Guilherme Tiosso <guilherme.tiosso@gmail.com>
2019-04-08 23:16:56 -0300

    SellerList View Design and Controller

---
+++ src/gui/SellerListController.java
@@ -0,0 +1,53 @@
+package gui;
+
+import java.net.URL;
+import java.sql.Date;
+import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import model.entities.Department;
+import model.entities.Seller;
+
+public class SellerListController implements Initializable {
+
+	@FXML
+	private TableView<Seller> tableViewSeller;
+
+	@FXML
+	private TableColumn<Seller, Integer> tableColumnId;
+	
+	@FXML
+	private TableColumn<Seller, String> tableColumnName;
+	
+	@FXML
+	private TableColumn<Seller, String> tableColumnEmail;
+	
+	@FXML
+	private TableColumn<Seller, Date> tableColumnBirthDate;
+	
+	@FXML
+	private TableColumn<Seller, Double> tableColumnBaseSalary;
+	
+	@FXML
+	private TableColumn<Seller, Department> tableColumnDepartment;
+
+	@FXML
+	private Button btNew;
+	
+	@FXML
+	public void onBtNewAction() {
+		System.out.println("onBtNewAction");
+	}
+	
+	@Override
+	public void initialize(URL uri, ResourceBundle rb) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+
+}
