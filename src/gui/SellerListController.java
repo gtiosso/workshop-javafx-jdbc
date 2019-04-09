66604ea3b80532bb9af724b2afe2d056aa189b5d
Guilherme Tiosso <guilherme.tiosso@gmail.com>
2019-04-08 23:36:20 -0300

    SellerList Controller

--- src/gui/SellerListController.java
+++ src/gui/SellerListController.java
@@ -4,11 +4,14 @@ import java.net.URL;
 import java.sql.Date;
 import java.util.ResourceBundle;
 
+import application.Main;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.stage.Stage;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -43,10 +46,27 @@ public class SellerListController implements Initializable {
 		System.out.println("onBtNewAction");
 	}
 	
+	// Metodo para iniciar o comportamento das colunas da TableView
+	private void initializeNodes() {
+		tableColumnId.setCellValueFactory(new PropertyValueFactory<>("id"));
+		tableColumnName.setCellValueFactory(new PropertyValueFactory<>("name"));
+		tableColumnEmail.setCellValueFactory(new PropertyValueFactory<>("email"));
+		tableColumnBirthDate.setCellValueFactory(new PropertyValueFactory<>("birthDate"));
+		tableColumnBaseSalary.setCellValueFactory(new PropertyValueFactory<>("baseSalary"));
+		tableColumnDepartment.setCellValueFactory(new PropertyValueFactory<>("department"));
+
+		
+		// Pegando a referencia do Stage Principal,
+		// atraves de um Downcasting da SuperClasse Window para a Classe Stage
+		Stage stage = (Stage) Main.getMainScene().getWindow();
+
+		// Redimensionando tamanho da TableView para acompnhar a tela principal
+		tableViewSeller.prefHeightProperty().bind(stage.heightProperty());
+	}
+	
 	@Override
 	public void initialize(URL uri, ResourceBundle rb) {
-		// TODO Auto-generated method stub
-		
+		initializeNodes();
 	}
 	
 
